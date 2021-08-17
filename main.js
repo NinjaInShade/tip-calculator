0724c9ce3efa4d80e3d95093ac3babe1745e3f65
Leon Michalak <leonmichalak6@gmail.com>
2021-08-17 17:46:43 +0100

    Reset button works

--- main.js
+++ main.js
@@ -13,11 +13,34 @@ const tip25 = document.getElementById('tip25');
 const tip50 = document.getElementById('tip50');
 const tipCustom = document.getElementById('input-field-tip');
 
+let activeTip = tip5;
+
 const numberOfPeopleInput = document.getElementById('numberOfPeople');
 
 const tipAmount = document.getElementById('tipAmount');
 const totalPrice = document.getElementById('totalPrice');
 
-function resetBill(event) {
-  event.preventDefault();
+// Adds active state to tip button pressed
+function selectTip(num) {
+  const pressedTip = document.getElementById(`tip${num}`);
+
+  //   Remove previous activeTip's active class
+  activeTip.classList.remove('input-tip-active');
+
+  //   Set new active tip
+  activeTip = pressedTip;
+
+  pressedTip.classList.add('input-tip-active');
+}
+
+// Reset all inputs and outputs
+function resetBill() {
+  totalBillInput.value = '';
+  numberOfPeopleInput.value = '';
+  tipCustom.value = '';
+
+  activeTip.classList.remove('input-tip-active');
+
+  tipAmount.innerText = '$0.00';
+  totalPrice.innerText = '$0.00';
 }
